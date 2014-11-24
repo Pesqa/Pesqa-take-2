d2fbdbf843162b5be1953866ff97bd863368fa83
Bruce Wade <developer@warplydesigned.com>
2014-11-24 07:23:36 -0800

    Initial trip app setup

---
+++ server/trip/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class FishType(object):
+    pass
+
+class TripImage(object):
+    pass
+
+class MasterTrip(object):
+    pass
+
+class TripInstance(object):
+    pass
+
+class TripBooking(object):
+    pass
